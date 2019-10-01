00f355924401e7dbb5585e0c43667b0c946dc62c
dpochernin <d.pochernin@gmail.com>
2019-10-01 11:17:52 +0300

    @elpiankova Доделки по задаче 7 переделал сортировку Домашнее задание к уроку 3

--- Homework_3_7.py
+++ Homework_3_7.py
@@ -7,27 +7,24 @@ But at least we are not what we used to be
 """
 # Посчитайте сколько слов в тексте (разбейте на слова методом строк split)
 words = s.splitlines()
-print(words)
 for word in range(len(words)):
     words.extend(words[0].strip().split(' '))
     words.remove(words[0])
-    print(words)
 print('in text are ', len(words), 'words')
 
 # Удалите знаки препинания в списке слов (пройдитесь циклом все слова и удалите методом strip знаки препинания)
 for word in range(len(words)):
     for char in [punct for punct in string.punctuation]:
         words[word] = (words[word].strip(char))
-print(words)
+print('Punctuation removed', words, sep='\n')
 
 # Выведите слова в алфавитном порядке (найдите метод списка, который сортирует)
-words.sort()
-print(words)
-
+words = sorted(words, key=str.lower)   # переделал сорт, сделал реально по алфавиту
+print('Sorted by alphabet', words, sep='\n')
 # Посчитать, сколько раз встречается каждое слово
 # Слова с большой буквы и с маленькой это все равно одно и тоже слово
 dict_words = {word.lower(): 0 for word in words}
 for word in words:
     for k in dict_words.keys():
         if word.lower() == k: dict_words[k] += 1
-print(dict_words)
+print('Words counter:', dict_words, sep='\n')
